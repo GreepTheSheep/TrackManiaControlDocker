5fa5b940fd2b3863b1b5eaff74f918527c3d554c
Matthieu <greep@greep.fr>
2026-08-11 13:47:30 +0100

    Updated Dockerfile and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,18 @@
 FROM php:8.2-alpine
 
+ARG VERSION \
+    BUILD_DATE \
+    REVISION
+
+LABEL org.opencontainers.image.title="TrackManiaControl" \
+      org.opencontainers.image.description="Fork of Maniacontrol adapted for PHP 8.2 and later and TrackMania 2020" \
+      org.opencontainers.image.authors="Greep <greep@greep.fr>" \
+      org.opencontainers.image.vendor="Greep" \
+      org.opencontainers.image.licenses="Unlicence" \
+      org.opencontainers.image.version=${VERSION} \
+      org.opencontainers.image.created=${BUILD_DATE} \
+      org.opencontainers.image.revision=${REVISION}
+
 RUN apk add --no-cache curl-dev libzip-dev libcurl libxml2-dev zlib-dev oniguruma-dev git xmlstarlet $PHPIZE_DEPS
 
 # Install PHP dependencies
@@ -12,14 +25,30 @@ RUN docker-php-ext-install \
 
 RUN pecl install channel://pecl.php.net/xmlrpc-1.0.0RC3 && docker-php-ext-enable xmlrpc
 
+RUN true \
+    && set -eux \
+    && addgroup -g 9999 maniacontrol \
+    && adduser -u 9999 -Hh /controller -G maniacontrol -s /sbin/nologin -D maniacontrol \
+    && install -d -o maniacontrol -g maniacontrol -m 775 /controller \
+    && true
+
+USER maniacontrol
 RUN git clone https://git.virtit.fr/beu/TrackManiaControl.git /controller
+USER root
 
 RUN apk del git $PHPIZE_DEPS
 
-WORKDIR /controller/
+WORKDIR /controller
 
-COPY entrypoint.sh /controller/
+COPY --chown=0755 ./entrypoint.sh /controller/
 
-RUN chmod +x entrypoint.sh
+RUN true \
+    && set -eux \
+    && rm -rf .git README.md \
+    && chown maniacontrol:maniacontrol -Rf /controller \
+    && true
+
+USER maniacontrol
 
 ENTRYPOINT [ "/controller/entrypoint.sh" ]
+CMD [ "php", "./ManiaControl.php" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,49 @@
 #!/bin/sh
 
+set -e
+
 echo "* Beu's TrackMania Control Docker Image"
-echo "* Directory: $(pwd)"
 
-if [[ ! -e configs/server.xml ]]; then
-    echo "* Config file does not exist. Creating one using environment variables"
-
-    if [[ -z "${SERVER_HOST}" ]]; then
-        echo "* SERVER_HOST required environment variable isn't defined."
-        exit 1
-    fi
-    if [[ -z "${DATABASE_HOST}" ]]; then
-        echo "* DATABASE_HOST required environment variable isn't defined."
-        exit 1
-    fi
-    if [[ -z "${MASTERADMIN_LOGIN}" ]]; then
-        echo "* MASTERADMIN_LOGIN required environment variable isn't defined."
-        exit 1
+if [ "$1" = 'php' ] && [ "$2" = './ManiaControl.php' ]; then
+    if [ "$(id -u)" = '0' ]; then
+        chown -R maniacontrol /controller
+        exec su-exec maniacontrol "$0" "$@"
     fi
 
-    xmlstarlet ed \
-    -u '/maniacontrol/server/host' -v "${SERVER_HOST}" \
-    -u '/maniacontrol/server/port' -v "${SERVER_PORT:-5000}" \
-    -u '/maniacontrol/server/user' -v "${SERVER_SUPERADMIN_USER:-SuperAdmin}" \
-    -u '/maniacontrol/server/pass' -v "${SERVER_SUPERADMIN_PASS:-SuperAdmin}" \
-    -u '/maniacontrol/database/host' -v "${DATABASE_HOST}" \
-    -u '/maniacontrol/database/port' -v "${DATABASE_PORT:-3306}" \
-    -u '/maniacontrol/database/user' -v "${DATABASE_USER:-maniacontrol}" \
-    -u '/maniacontrol/database/pass' -v "${DATABASE_PASS:-maniacontrol}" \
-    -u '/maniacontrol/database/name' -v "${DATABASE_NAME:-maniacontrol}" \
-    -u '/maniacontrol/masteradmins/login' -v "${MASTERADMIN_LOGIN}" \
-    < configs/server.default.xml > configs/server.xml
+    if [[ ! -e configs/server.xml ]]; then
+        echo "* Config file does not exist. Creating one using environment variables"
+
+        if [[ -z "${SERVER_HOST}" ]]; then
+            echo "* SERVER_HOST required environment variable isn't defined."
+            exit 1
+        fi
+        if [[ -z "${DATABASE_HOST}" ]]; then
+            echo "* DATABASE_HOST required environment variable isn't defined."
+            exit 1
+        fi
+        if [[ -z "${MASTERADMIN_LOGIN}" ]]; then
+            echo "* MASTERADMIN_LOGIN required environment variable isn't defined."
+            exit 1
+        fi
+
+        xmlstarlet ed \
+        -u '/maniacontrol/server/host' -v "${SERVER_HOST}" \
+        -u '/maniacontrol/server/port' -v "${SERVER_PORT:-5000}" \
+        -u '/maniacontrol/server/user' -v "${SERVER_SUPERADMIN_USER:-SuperAdmin}" \
+        -u '/maniacontrol/server/pass' -v "${SERVER_SUPERADMIN_PASS:-SuperAdmin}" \
+        -u '/maniacontrol/database/host' -v "${DATABASE_HOST}" \
+        -u '/maniacontrol/database/port' -v "${DATABASE_PORT:-3306}" \
+        -u '/maniacontrol/database/user' -v "${DATABASE_USER:-maniacontrol}" \
+        -u '/maniacontrol/database/pass' -v "${DATABASE_PASS:-maniacontrol}" \
+        -u '/maniacontrol/database/name' -v "${DATABASE_NAME:-maniacontrol}" \
+        -u '/maniacontrol/masteradmins/login' -v "${MASTERADMIN_LOGIN}" \
+        < configs/server.default.xml > configs/server.xml
+    else
+        echo "* Config file exists, using existing one"
+    fi
+    echo "* Running TrackManiaControl"
 else
-    echo "* Config file exists, using existing one"
+    echo "* Running $@"
 fi
 
-echo "* Running TrackManiaControl"
-php ManiaControl.php
+exec "$@"
